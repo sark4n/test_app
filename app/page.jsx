a6075661a2c3c3bd302c5b402fcc8244b9c445c8
Sark4n <ades89@gmail.com>
2023-06-14 22:26:37 -0400

    typewritter&colorBlue

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,30 @@
+'use client'
 import Feed from "@components/Feed"
+
+import { Typewriter } from 'react-simple-typewriter'
 
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Asesoría para 
       <br className="max-md:hidden"/>
-      <span className="orange_gradient text-center"> AI-Powerd Prompts</span>
+      <span className="blue_gradient text-center">
+        <Typewriter
+          words={[' Emprendedores', ' Creadores', ' Ti!']}
+          loop={0}
+          cursor
+          cursorStyle='_'
+          typeSpeed={120}
+          deleteSpeed={50}
+          delaySpeed={1000}
+          // onLoopDone={handleDone}
+          // onType={handleType}
+        /></span>
       </h1>
       <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      Has crecer tu negocio con las herramientas que tenemos para ofrecerte
     </p>
 
     <Feed/>
